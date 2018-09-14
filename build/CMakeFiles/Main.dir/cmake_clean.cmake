72908570b9a3722705865fb445a7b9afb5d0bc8a
thais <thaiscamachoo@gmail.com>
2018-09-13 22:56:20 -0300

    fixed up

---
+++ build/CMakeFiles/Main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Main.dir/src/Decoder.cpp.o"
+  "CMakeFiles/Main.dir/src/Interpreter.cpp.o"
+  "CMakeFiles/Main.dir/src/Machine.cpp.o"
+  "CMakeFiles/Main.dir/src/Main.cpp.o"
+  "Main.pdb"
+  "Main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
